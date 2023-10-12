b72935a9f9639258081f7bb0c8537a4408f84ec8
Denis S Dujota <denisdujota@gmail.com>
2023-10-11 20:14:47 -0500

    my solution

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// Write your DOM code here!
+/* eslint-disable prefer-destructuring */
+/* eslint-disable prettier/prettier */
+// UI variables
+const switcher = document.querySelector('.switcher');
+
+// Event Handler
+function handleColorSwitch(e) {
+  const color = e.target.classList[0];
+  if(document.body.style.backgroundColor !== color){
+    document.body.style.backgroundColor = color;
+  }
+}
+
+// Event Listeners
+switcher.addEventListener('click', handleColorSwitch);
